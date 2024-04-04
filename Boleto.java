92823ed3ec7e5749ff659de933a03ec022b01336
Polyana150 <146875281+Polyana150@users.noreply.github.com>
2024-04-04 20:57:10 -0300

    Add files via upload

---
+++ Boleto.java
@@ -0,0 +1,17 @@
+public class Boleto {
+    double valor;
+    String codigodebarras;
+    String vencimento;
+    public void emitirBoleto( double valor,
+    String codigodebarras,
+    String vencimento){
+        System.out.println("Emitindo novo boleto:" + valor +"," + vencimento + "," + codigodebarras);
+        // variavelBoleto.valor = valor;
+        // variavelBoleto.vencimento = vencimento;
+        // variavelBoleto.codigodebarras = codigodebarras ;
+    }
+       
+    public void cancelarBoleto(){}
+
+    public void receberBoleto(){}
+}
